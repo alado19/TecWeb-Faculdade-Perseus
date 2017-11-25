03b086cce76d6cc6c31eff14775f84a3df037c7d
alado19 <30847715+alado19@users.noreply.github.com>
2017-11-25 17:00:23 -0200

    Update settings.py

--- lmsPerseus/settings.py
+++ lmsPerseus/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'dyue1p$1*ino@wmiq622z++cq@6s&lwd%&qh!g!_9120+yy*ur'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://site-faculdade-perseus.azurewebsites.net']
+ALLOWED_HOSTS = []
 
 
 # Application definition
